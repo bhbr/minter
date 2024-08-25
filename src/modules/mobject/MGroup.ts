1d61831e5bfd405cc5391672d260d0da8ab9ff89
Ben Hambrecht <bhbr@users.noreply.github.com>
2024-08-25 16:07:40 +0200

    commented MGroup

--- src/modules/mobject/MGroup.ts
+++ src/modules/mobject/MGroup.ts
@@ -1,10 +1,13 @@
 import { Mobject } from './Mobject'
 
 export class MGroup extends Mobject {
-
+/*
+MGroup (a group of mobjects) is essentially a synonym for Mobject.
+*/
 	statefulSetup() {
 		super.statefulSetup()
-		// children may have been set as constructor args
+		// Children may have been set as constructor arguments
+		// and need not be added explicitly, this setup does it.
 		for (let submob of this.children) {
 			this.add(submob)
 		}
