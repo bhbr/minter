1cbe6d712f127670f987aca7b9c9f6f89c760160
Ben Hambrecht <bhbr@users.noreply.github.com>
2024-09-14 21:48:14 +0200

    documenting Circle

--- src/modules/shapes/Circle.ts
+++ src/modules/shapes/Circle.ts
@@ -2,7 +2,9 @@ import { CircularArc } from './CircularArc'
 import { TAU } from '../helpers/math'
 
 export class Circle extends CircularArc {
-
+	/*
+	A Circle is a CircularArc whose angle equals TAU = 2*PI.
+	*/
 	fixedArgs(): object {
 		return Object.assign(super.fixedArgs(), {
 			angle: TAU,
