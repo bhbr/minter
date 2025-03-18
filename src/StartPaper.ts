289cc4b5c2859c1a16fc5f52cb1df53720208081
Ben Hambrecht <bhbr@users.noreply.github.com>
2025-03-18 06:20:33 +0100

    removed testing code

--- src/StartPaper.ts
+++ src/StartPaper.ts
@@ -9,16 +9,3 @@ import { log } from './core/functions/logging'
 export const TESTING: boolean = false
 export class StartPaper extends DemoPaper { }
 export const paper = new StartPaper()
-
-let life = new GameOfLife({
-	grid:  new Grid({
-		anchor: [100, 100],
-		width: 100,
-		height: 75,
-		cellSize: 10,
-		drawGridLines: false
-	})
-})
-
-paper.addToContent(life)
-window.setInterval(life.evolve.bind(life), 1000)
